b767244dd854809ed7e52a833063375b6499a281
Davey Price <davey@stumblorpinball.com>
2024-05-22 18:08:22 +0100

    changed function defs

--- src/AnimatedGIF.cpp
+++ src/AnimatedGIF.cpp
@@ -18,7 +18,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //===========================================================================
-#include "Arduino.h"
 #include "AnimatedGIF.h"
 
 // Here is all of the actual code...
