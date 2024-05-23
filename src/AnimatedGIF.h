f9eeb8cd7d373b6e473e3f359aaf357b59fb358e
Davey Price <davey@stumblorpinball.com>
2024-05-23 12:50:05 +0100

    .

--- src/AnimatedGIF.h
+++ src/AnimatedGIF.h
@@ -155,7 +155,7 @@ typedef struct gif_draw_tag
 // Callback function prototypes
 typedef int32_t (GIF_READ_CALLBACK)(GIFFILE *pFile, uint8_t *pBuf, int32_t iLen);
 typedef int32_t (GIF_SEEK_CALLBACK)(GIFFILE *pFile, int32_t iPosition);
-typedef std::function<void(GIFDRAW *)>GIF_DRAW_CALLBACK;
+typedef std::function<void *(GIFDRAW *)>GIF_DRAW_CALLBACK;
 //typedef void (GIF_DRAW_CALLBACK)(GIFDRAW *pDraw);
 typedef std::function<void * (const char *, int32_t *)>GIF_OPEN_CALLBACK;
 //typedef void * (GIF_OPEN_CALLBACK)(const char *szFilename, int32_t *pFileSize);
